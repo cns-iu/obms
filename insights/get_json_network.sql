570e7e8849c8e1b597c9cf0abc3a137fdc24e1af
SrilekhaMalraju1 <97492747+SrilekhaMalraju1@users.noreply.github.com>
2022-08-03 10:27:38 -0400

    Update get_json_network.sql

--- insights/get_json_network.sql
+++ insights/get_json_network.sql
@@ -1,29 +1,27 @@
--- \set dictionary dict_20180706
--- \set edges net_pubmed_epilepsy_20180706.edges
--- \set edgetype is_metric
-
 WITH edges AS (
-  SELECT source, target,
-    -- normalize the proximity to 1 - 1,000 (1,000 being the highest)
-    ROUND((
-      1 + proximity / (SELECT max(proximity) FROM :edges) * 999
-    )::NUMERIC, 2) as weight
-  FROM :edges
-  WHERE :edgetype IS TRUE
-), nodes AS (
-  SELECT id, token as label, id_parent as parent
-  FROM dictionaries.:dictionary
-  WHERE id IN (SELECT source FROM edges UNION SELECT target FROM edges)
-  ORDER BY id
-)
--- Compile nodes and edges into a JSON object for use in map4sci
+	  SELECT journal1 as source, journal2 as target,
+	    -- normalize the proximity to 1 - 1,0000 (1,000 being the highest)
+	
+	       (rascore_avg*count_repeatingjournals)
+	as weight
+	  FROM journal_journal_relation
+	
+	), nodes AS (
+	  SELECT journal1 as id, journal_name1 as label
+	  FROM journal_journal_relation
+	  Union
+Select journal2, journal_name2 from journal_journal_relation 
+
+	
+	)
+
 SELECT jsonb_pretty(jsonb_object_agg(field, val))
-FROM (
-  (SELECT 'nodes' AS field, jsonb_agg(jsonb_strip_nulls(jsonb_build_object(
-    'id', id, 'label', label, 'parent', parent
-  ))) AS val FROM nodes)
-  UNION ALL
-  (SELECT 'edges' AS field, jsonb_agg(jsonb_strip_nulls(jsonb_build_object(
-    'source', source, 'target', target, 'weight', weight
-  ))) AS val FROM edges)
-) AS a;
+	FROM (
+	  (SELECT 'nodes' AS field, jsonb_agg(jsonb_strip_nulls(jsonb_build_object(
+	    'id', id, 'label', label
+	  ))) AS val FROM nodes)
+	  UNION ALL
+	  (SELECT 'edges' AS field, jsonb_agg(jsonb_strip_nulls(jsonb_build_object(
+	    'source', source, 'target', target, 'weight', weight
+	  ))) AS val FROM edges)
+	) AS a;
