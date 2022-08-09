bc60288f0ce4224896a26beb72763331fc1368d6
SrilekhaMalraju1 <97492747+SrilekhaMalraju1@users.noreply.github.com>
2022-08-09 14:07:47 -0400

    Update get_json_network.sql

--- insights/get_json_network.sql
+++ insights/get_json_network.sql
@@ -1,26 +1,33 @@
-WITH edges AS (
-	  SELECT journal1 as source, journal2 as target,
-	    
-	       (rascore_avg*count_repeatingjournals)
-	as weight
-	  FROM journal_journal_relation
-	
-	), nodes AS (
-	  SELECT journal1 as id, journal_name1 as label
-	  FROM journal_journal_relation
-	  Union
-Select journal2, journal_name2 from journal_journal_relation 
+create materialized view edges as select source,target,weight from journal_journal_top50;
 
-	
-	)
+create materialized view nodes as SELECT source      AS id, 
+       journal_name1 AS label 
+FROM   journal_journal_top50 
+UNION 
+SELECT target, 
+       journal_name2 
+FROM   journal_journal_top50;
 
-SELECT jsonb_pretty(jsonb_object_agg(field, val))
-	FROM (
-	  (SELECT 'nodes' AS field, jsonb_agg(jsonb_strip_nulls(jsonb_build_object(
-	    'id', id, 'label', label
-	  ))) AS val FROM nodes)
-	  UNION ALL
-	  (SELECT 'edges' AS field, jsonb_agg(jsonb_strip_nulls(jsonb_build_object(
-	    'source', source, 'target', target, 'weight', weight
-	  ))) AS val FROM edges)
-	) AS a;
+
+CREATE materialized VIEW json_journal_relation 
+AS SELECT Jsonb_pretty(Jsonb_object_agg(field, val)) 
+   FROM   ((SELECT 'nodes' 
+                          AS 
+                           field, 
+                   Jsonb_agg(Jsonb_strip_nulls(Jsonb_build_object('id', id, 
+                                               'label' 
+                                               , label 
+                                               ) 
+                             )) AS 
+                   val 
+            FROM   nodes) 
+           UNION ALL 
+           (SELECT 'edges'                                                   AS 
+                   field, 
+                   Jsonb_agg(Jsonb_strip_nulls(Jsonb_build_object('source', 
+                                               source, 
+                                               'target', 
+                                               target, 
+                                                         'weight', weight))) AS 
+                   val 
+            FROM   edges)) AS a; 
