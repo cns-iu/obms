7e38fb2e5e601bbb1930af4e839dd53f5db2943a
SrilekhaMalraju1 <97492747+SrilekhaMalraju1@users.noreply.github.com>
2022-08-09 21:56:06 -0400

    Update prescripts_journal_journal_top50.sql

--- scripts/prescripts_journal_journal_top50.sql
+++ scripts/prescripts_journal_journal_top50.sql
@@ -1,1 +1,17 @@
-
+DROP MATERIALIZED VIEW IF EXISTS journal_journal_relation;
+CREATE materialized VIEW journal_journal_relation 
+AS SELECT a.journal_nlmuniqueid                  AS journal1, 
+          a.iso_abbrev                           AS journal_name1, 
+          b.journal_nlmuniqueid                  AS journal2, 
+          b.iso_abbrev                           AS journal_name2, 
+          ( Avg(rascore :: INTEGER) ) :: INTEGER AS rascore_avg, 
+          Count(DISTINCT b.pmid)                 AS count_repeatingjournals 
+   FROM   medlinex_rascore_edges m 
+          join medline_master a 
+            ON a.pmid = m.pmid 
+          join medline_master b 
+            ON b.pmid = m.pmid2 
+   GROUP  BY a.journal_nlmuniqueid, 
+             b.journal_nlmuniqueid, 
+             a.iso_abbrev, 
+             b.iso_abbrev; 
