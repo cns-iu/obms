905e71a2a28814d8e5693c97649ccc81dac15f78
SrilekhaMalraju1 <97492747+SrilekhaMalraju1@users.noreply.github.com>
2022-08-09 21:39:16 -0400

    Add files via upload

---
+++ scripts/get_json_network.sql
@@ -0,0 +1,29 @@
+-- \set dictionary dict_20180706
+-- \set edges net_pubmed_epilepsy_20180706.edges
+-- \set edgetype is_metric
+
+WITH edges AS (
+  SELECT source, target,
+    -- normalize the proximity to 1 - 1,000 (1,000 being the highest)
+    ROUND((
+      1 + proximity / (SELECT max(proximity) FROM :edges) * 999
+    )::NUMERIC, 2) as weight
+  FROM :edges
+  WHERE :edgetype IS TRUE
+), nodes AS (
+  SELECT id, token as label, id_parent as parent
+  FROM dictionaries.:dictionary
+  WHERE id IN (SELECT source FROM edges UNION SELECT target FROM edges)
+  ORDER BY id
+)
+-- Compile nodes and edges into a JSON object for use in map4sci
+SELECT jsonb_pretty(jsonb_object_agg(field, val))
+FROM (
+  (SELECT 'nodes' AS field, jsonb_agg(jsonb_strip_nulls(jsonb_build_object(
+    'id', id, 'label', label, 'parent', parent
+  ))) AS val FROM nodes)
+  UNION ALL
+  (SELECT 'edges' AS field, jsonb_agg(jsonb_strip_nulls(jsonb_build_object(
+    'source', source, 'target', target, 'weight', weight
+  ))) AS val FROM edges)
+) AS a;
