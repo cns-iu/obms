d8b5dac876f0b09e9e484dc56a49aec95134bfbc
Bruce W. Herr II <bherr@indiana.edu>
2023-06-22 14:07:37 -0400

    Updates for the v4 run

--- map4sci/scripts/05-convert-networks.sh
+++ map4sci/scripts/05-convert-networks.sh
@@ -2,8 +2,6 @@
 source constants.sh
 set -ev
 
-NODES=./input-data/OBMS_v4_meshTopJour_tfidf_nodeList.csv.gz
-
 for network in $(ls ./input-data/*_edgeList.csv.gz)
 do
   dataset=$(basename "${network%_edgeList.csv.gz}")
@@ -11,19 +9,26 @@ do
   echo
   OUT=./datasets/${dataset}_BatchTree
   OUT2=./datasets/${dataset}_DELG
-  OUT3=./datasets/${dataset}_CG
-  mkdir -p $OUT $OUT2 $OUT3
+  # OUT3=./datasets/${dataset}_CG
+  mkdir -p $OUT $OUT2 # $OUT3
+
+  V=$(echo $network | cut -d '_' -f 2)
+  if [ "${V}" == "v4" ]; then
+    NODES=./input-data/OBMS_v4_filtered_tfidf_nodeList.csv.gz
+  elif [ "${V}" == "v3" ]; then
+    NODES=./input-data/OBMS_v3_allJallM_tfidf_nodeList.csv.gz
+  fi
 
   if [ ! -e $OUT/network.dot ]
   then
     python3 src/convert-network.py ${NODES} ${network} $OUT/network.dot &> $OUT/convert.log.txt
     cp $OUT/network.dot $OUT2/network.dot
-    cp $OUT/network.dot $OUT3/network.dot
+    # cp $OUT/network.dot $OUT3/network.dot
   fi
   if [ ! -e $OUT/config.sh ]
   then
     cat src/config-template.sh | perl -pe "s/--DATASET--/${dataset}_BatchTree/g" > $OUT/config.sh
     cat src/config-template.sh | perl -pe "s/--DATASET--/${dataset}_DELG/g;s/BatchTree/DELG/g" > $OUT2/config.sh
-    cat src/config-template.sh | perl -pe "s/--DATASET--/${dataset}_CG/g;s/BatchTree/CG/g" > $OUT3/config.sh
+    # cat src/config-template.sh | perl -pe "s/--DATASET--/${dataset}_CG/g;s/BatchTree/CG/g" > $OUT3/config.sh
   fi
 done
